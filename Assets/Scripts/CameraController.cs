25c02e62a7889773ffe9145131e8b7e5d15b0a1f
daniimudaa <31983241+daniimudaa@users.noreply.github.com>
2018-05-12 23:09:22 +1000

    movement, jump, camera done

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour 
+{
+	//camera look at variables
+	public Transform target;
+	public Vector3 offset;
+	public bool useOffsetValues;
+
+	//camera rotation variables
+	public float rotateSpeed;
+
+	public Transform pivot;
+
+	void Start () 
+	{
+		if (!useOffsetValues) 
+		{
+			//camera position offset
+			offset = target.position - transform.position;
+		}
+
+		pivot.transform.position = target.transform.position;
+		pivot.transform.parent = target.transform;
+
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	void Update () 
+	{
+		//Get the X position of the mouse & rotate target
+		float horizontal = Input.GetAxis ("Mouse X") * rotateSpeed;
+		target.Rotate (0, horizontal, 0);
+
+		//get the Y position of Mouse & rotate the pivot
+		float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+		pivot.Rotate (-vertical, 0, 0);
+
+		//move the camera based on current target rotation & offset
+		float desiredYAngle = target.eulerAngles.y;
+		float desiredXAngle = pivot.eulerAngles.x;
+		Quaternion rotation = Quaternion.Euler (desiredXAngle, desiredYAngle, 0);
+		transform.position = target.position - (rotation * offset);
+
+		if (transform.position.y < target.position.y) 
+		{
+			transform.position = new Vector3 (transform.position.x, target.position.y, transform.position.z);
+		}
+
+		//camera position from player, look at the player
+		transform.LookAt (target);
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,17 +18,24 @@ public class Player : MonoBehaviour
 	
 	void Update () 
 	{
-		moveDirection = new Vector3 (Input.GetAxis ("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis ("Vertical") * moveSpeed);
+		//character movement
+		float yStore = moveDirection.y;
+		moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis ("Horizontal"));
+		moveDirection = moveDirection.normalized * moveSpeed; 
+		moveDirection.y = yStore;
 
 		//jump only once - when grounded
 		if (controller.isGrounded) 
 		{
+			moveDirection.y = 0f;
+
 			if (Input.GetButtonDown ("Jump")) 
 			{
 				moveDirection.y = jumpForce;
 			}
 		}
 
+		//add jump force
 		moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
 		controller.Move (moveDirection * Time.deltaTime);
 	}
