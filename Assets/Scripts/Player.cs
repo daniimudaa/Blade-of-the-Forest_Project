4a7bc5840e2df2959ef7c66212a328fbde7cfa6c
daniimudaa <31983241+daniimudaa@users.noreply.github.com>
2018-05-12 21:53:04 +1000

    movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour 
+{
+	public float moveSpeed;
+	public float jumpForce;
+	public CharacterController controller;
 
-	// Use this for initialization
-	void Start () {
-		
+	private Vector3 moveDirection; 
+	public float gravityScale;
+
+	void Start () 
+	{
+		controller = GetComponent<CharacterController>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		moveDirection = new Vector3 (Input.GetAxis ("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis ("Vertical") * moveSpeed);
+
+		//jump only once - when grounded
+		if (controller.isGrounded) 
+		{
+			if (Input.GetButtonDown ("Jump")) 
+			{
+				moveDirection.y = jumpForce;
+			}
+		}
+
+		moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
+		controller.Move (moveDirection * Time.deltaTime);
 	}
 }
